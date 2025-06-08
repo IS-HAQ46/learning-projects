9fc2101b12977076d11b2daa2694446dc3f77f59
Is Haq Mathew Abraham <ishaqmatha15@gmail.com>
2025-06-08 08:37:14 +0530

    new code

--- linear_regression/multi_variable.py
+++ linear_regression/multi_variable.py
@@ -2,5 +2,12 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 from sklearn import linear_model
-df=pd.read_csv(hiring.csv)
+from word2number import w2n
+df=pd.read_csv("hiring.csv")
+df['experience'] = df['experience'].fillna(0)
+df['test_score(out of 10)'] = df['test_score(out of 10)'].fillna(df['test_score(out of 10)'].median())
+df['experience'] = df['experience'].apply(lambda x: w2n.word_to_num(str(x)) if pd.notnull(x) else x)
 print(df)
+reg=linear_model.LinearRegression()
+reg.fit(df[['experience','test_score(out of 10)','interview_score(out of 10)']],df['salary($)'])
+print(reg.predict([[2,8,9]]))
